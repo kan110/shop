93a5fd05598d7d85d1466fd18077aa2f08a22ca2
Jennifer <77340578+kan110@users.noreply.github.com>
2023-11-09 11:17:34 -0800

    update README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
-import { Divider, Typography, Box, Link } from "@mui/material"
+import { Divider, Typography, Link, Stack } from "@mui/material"
 
 export default function Footer () {
-    return <Box sx={{
+    return <Stack sx={{
         width: '1',
-        height: 50
+        height: 50,
     }}>
         <Divider/>
-        <Typography sx={{paddingY: 2, paddingX: 3}}>
-            View project on <Link href='https://github.com/kan110/shop' target='_blank' rel='noreferrer'>Github</Link>.
-        </Typography>
-    </ Box>
+        <Stack sx={{flexGrow: 1, justifyContent: 'center'}}>
+            <Typography sx={{paddingX: 3}}>
+                View project on <Link href='https://github.com/kan110/shop' target='_blank' rel='noreferrer'>GitHub</Link>.
+            </Typography>
+        </Stack>
+    </ Stack>
 }
